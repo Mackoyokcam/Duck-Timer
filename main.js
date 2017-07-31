172897414d9306289c1d3805ba4fc3fa9fe946b8
mackoyokcam <mark.hangarter@gmail.com>
2017-07-31 14:34:35 -0700

    added random# generator and added buttons to start, stop and reset timer.

--- main.js
+++ main.js
@@ -1,49 +1,98 @@
 'use strict';
 
 // Get this info from localStorage
-var name = 'Mark';
+var name = 'Johnny';
 var duckMessages = ['You can do it ' + name + '!',
-  'I believe in you ' + name + '!'];
+  'I believe in you ' + name + '!',
+  name + ' is taking over the world!',
+  'Nobody codes better than you ' + name];
 
 var minutes = 15;
 var seconds = 0;
+var timer;
+document.getElementById('timer').textContent = minutes + 'm : ' + seconds + 's';
+document.getElementById('done_button').disabled = true;
+document.getElementById('done_button').style.opacity = 0.5;
 
 // Get this info from localStorage in the future
 var messageInterval = 1;
+var intervalHolder = messageInterval;
 var theDuck = new Duck(duckMessages);
 
 function Duck(messages) {
   this.messages = messages;
+  this.previous = -5; // Previous message index
   this.displayMessage = function() {
     // Display a message to user
-    document.getElementById('duck_message').textContent = messages[0];
+    var rando = randomNumber(0, this.messages.length);
+    while(this.previous === rando) {
+      rando = randomNumber(0, this.messages.length);
+      console.log('pew');
+    }
+    document.getElementById('duck_message').textContent = this.messages[rando];
+    this.previous = rando;
   };
 }
 
-// Call function every 1 second
-var timer = setInterval(function() {
+function randomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
-  seconds--;
+// Stop the clock
+function handleStop() {
+  document.getElementById('done_button').disabled = true;
+  document.getElementById('done_button').style.opacity = 0.5;
+  document.getElementById('start_button').disabled = false;
+  document.getElementById('start_button').style.opacity = 1;
+  timer = clearInterval(timer);
+}
 
-  // 1 minute has has passed
-  if (seconds === -1) {
-    minutes--;
-    seconds = 59;
-    if (messageInterval === 0) {
-      // Send message to duck
-      theDuck.displayMessage();
-    }
-    messageInterval--;
-  }
-  // Time expired
-  if (minutes < 0) {
-    clearInterval(timer);
-    document.getElementById('timer').innerHTML = 'EXPIRED';
-
-    // Send info to summary page
-  }
-
-  // Every interval, send message to duck
-
+// Reset timer to 15 minutes, stop the clock
+function handleReset() {
+  minutes = 15;
+  seconds = 0;
+  timer = clearInterval(timer);
   document.getElementById('timer').textContent = minutes + 'm : ' + seconds + 's';
-}, 1000);
+  document.getElementById('done_button').disabled = true;
+  document.getElementById('done_button').style.opacity = 0.5;
+  document.getElementById('start_button').disabled = false;
+  document.getElementById('start_button').style.opacity = 1;
+}
+
+// Start event handler
+function handleStart() {
+  document.getElementById('start_button').disabled = true;
+  document.getElementById('start_button').style.opacity = 0.5;
+  document.getElementById('done_button').disabled = false;
+  document.getElementById('done_button').style.opacity = 1;
+
+  timer = setInterval(function() {
+    seconds--;
+    // 1 minute has has passed
+    if (seconds === -1) {
+      minutes--;
+      seconds = 59;
+      if (messageInterval === 0) {
+        // Send message to duck
+        theDuck.displayMessage();
+        messageInterval = intervalHolder + 1;
+      }
+      messageInterval--;
+    }
+    // Time expired
+    if (minutes < 0) {
+      clearInterval(timer);
+      document.getElementById('timer').innerHTML = 'EXPIRED';
+
+      // Send info to summary page
+    } else {
+      document.getElementById('timer').textContent = minutes + 'm : ' + seconds + 's';
+    }
+  }, 1000);
+}
+
+document.getElementById('start_button').addEventListener('click', handleStart);
+document.getElementById('done_button').addEventListener('click', handleStop);
+document.getElementById('reset_button').addEventListener('click', handleReset);
