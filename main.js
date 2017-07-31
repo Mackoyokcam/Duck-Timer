0b14b4f33f837e9711ed3c8e739c26163329f920
mackoyokcam <mark.hangarter@gmail.com>
2017-07-31 11:58:30 -0700

    Created basic 15m timer. added reset.css to all html docs

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+var minutes = 15;
+var seconds = 0;
+
+// Call function every 1 second
+var timer = setInterval(function() {
+
+  seconds--;
+
+  // 1 minute has has passed
+  if (seconds === -1) {
+    minutes--;
+    seconds = 59;
+  }
+  // Time expired
+  if (minutes < 0) {
+    clearInterval(timer);
+    document.getElementById('timer').innerHTML = 'EXPIRED';
+  }
+  document.getElementById('timer').textContent = minutes + 'm : ' + seconds + 's';
+}, 1000);
