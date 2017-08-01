01b402e0adfc838ec823ca097495626fb437c9e4
mstclair <stclairm@spu.edu>
2017-08-01 11:35:46 -0700

    commiting changes

--- main.js
+++ main.js
@@ -86,15 +86,16 @@ function handleStop() {
       title: 'Name',
       html:'<label>Solution: <input type="text" name="solution" value=""></label><br />',
       buttons: { Solved: 1 },
-      //focus: "input[name='fname']",
+      focus: 'input[name="solution"]',
       submit:function(e,v,m,f){
         console.log(f);
         e.preventDefault();
         $.prompt.close();
 
         // store in array
-        times[document.getElementById('userGoal').textContent] = f.solution;
+        times.goal[document.getElementById('userGoal').textContent] = f.solution;
         localStorage.times = JSON.stringify(times);
+        window.location.href = 'summary.html';
       }
     }
   };
