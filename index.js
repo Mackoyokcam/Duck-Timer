078962fa407b42ac24de9811b24ae06fb40befc0
mstclair <stclairm@spu.edu>
2017-08-02 09:53:03 -0700

    got enter button working on first page

--- index.js
+++ index.js
@@ -3,14 +3,19 @@
 
 
 function storeOnClick () {
-
   console.log('the function is working');
   var userName = document.getElementById('userName').value;
   localStorage.userName = JSON.stringify(userName);
+
+  window.location.href = 'main.html';
   //add the function to open next page
 
 }
 
-document.getElementById('userNameSubmitButton').addEventListener('click', storeOnClick);
 
-storeOnClick();
+
+
+
+
+
+document.getElementById('userNameSubmitButton').addEventListener('click', storeOnClick);
