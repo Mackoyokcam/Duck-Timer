f8aab291e7bc8eeca6152c79682c6067a1564982
mackoyokcam <mark.hangarter@gmail.com>
2017-08-02 14:21:29 -0700

    personalized notification

--- main.js
+++ main.js
@@ -16,12 +16,13 @@ function notifyMe() {
   else {
     console.log('notified!');
     var notification = new Notification('Notification title', {
-      icon: 'http://cdn.sstatic.net/stackexchange/img/logos/so/so-icon.png',
-      body: 'Hey there! You\'ve been notified!',
+      icon: 'chromeExt/imgs/notifcation_icon.png',
+      body: 'Hey, you have ' + minutes + ' minutes remaining! Click me to add 5 minutes.',
     });
 
     notification.onclick = function () {
-      window.open('http://stackoverflow.com/a/13328397/1269037');
+      minutes += 5;
+      window.open('main.html');
     };
   }
 }
